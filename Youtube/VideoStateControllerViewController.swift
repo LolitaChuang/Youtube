ced4c1f4409a6ec8ee9c83169b711dde057f18ed
lolitachuang <lolitachuang@gmail.com>
2017-01-24 01:21:20 +0800

    1. add a state controller for preparation 2. add video model

---
+++ Youtube/VideoStateControllerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VideoStateControllerViewController.swift
+//  Youtube
+//
+//  Created by Lolita Chuang on 2017/1/23.
+//  Copyright © 2017年 Child Woman. All rights reserved.
+//
+
+import UIKit
+
+class VideoStateControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // ask for json file and prepare for Modal
+        
+    }
+
+}
