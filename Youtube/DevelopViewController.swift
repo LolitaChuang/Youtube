933e0ee4de637b515ffdfd840403081cc6f4bed9
lolitachuang <lolitachuang@gmail.com>
2017-04-08 14:29:52 +0800

    develop branch for Git test : initial revision

---
+++ Youtube/DevelopViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DevelopViewController.swift
+//  Youtube
+//
+//  Created by Lolita Chuang on 08/04/2017.
+//  Copyright © 2017 Child Woman. All rights reserved.
+//
+
+import UIKit
+
+class DevelopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("develop branch for Git test")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
