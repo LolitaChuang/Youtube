db11ed0ee229ac8bf740edc73377d43034439d5a
lolitachuang <lolitachuang@gmail.com>
2017-04-09 02:31:12 +0800

    feature branch : commit 4

--- Youtube/FeatureViewController.swift
+++ Youtube/FeatureViewController.swift
@@ -16,6 +16,7 @@ class FeatureViewController: UIViewController {
         print("feature branch : commit 1");
         print("feature branch : commit 2");
         print("feature branch : commit 3");
+        print("feature branch : commit 4");
     }
 
     override func didReceiveMemoryWarning() {
