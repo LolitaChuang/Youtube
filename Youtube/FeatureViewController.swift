733d7b94a239e91ed3ddf8f2b4c99d4497c3315c
lolitachuang <lolitachuang@gmail.com>
2017-04-08 14:21:53 +0800

    feature branch for Git test : initial revision

---
+++ Youtube/FeatureViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FeatureViewController.swift
+//  Youtube
+//
+//  Created by Lolita Chuang on 08/04/2017.
+//  Copyright © 2017 Child Woman. All rights reserved.
+//
+
+import UIKit
+
+class FeatureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("feature branch for Git test");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
