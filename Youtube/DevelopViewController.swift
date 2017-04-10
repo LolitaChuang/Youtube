06b006903e07271e864ccb88ea1d97048d3411df
Lolita Chuang <childwomanlolita@gmail.com>
2017-04-10 22:15:59 +0800

    調整資料夾結構 & staging multiple files

--- Youtube/DevelopViewController.swift
+++ Youtube/DevelopViewController.swift
@@ -14,6 +14,7 @@ class DevelopViewController: UIViewController {
         super.viewDidLoad()
         print("develop branch for Git test")
         print("develop branch : commit 1")
+        print("develop branch : commit for staging")
     }
 
     override func didReceiveMemoryWarning() {
